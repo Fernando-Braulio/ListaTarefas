6e6a5126d20c1eab1c81e0aac6b20333b78061d7
Fernando-Braulio <fernandobraulio@outlook.com.br>
2020-06-14 17:09:50 -0300

    LOGICA PARA EXECUTAR A EXCLUSAO AO DESLIZAR O REGISTRO E TER A POSIBILIDADE DE DESFAZER A EXCLUSAO USANDO SNACKNAR

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,8 @@ class _HomeState extends State<Home> {
   final _toDoController = TextEditingController();
 
   List _toDoList = [];
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
 
   @override
   void initState() {
@@ -108,7 +110,8 @@ class _HomeState extends State<Home> {
   }
 
   //Funcao para trazer os itens na lista
-  Widget buildItem(context, index) {
+  //WIDGET DISMISSIBLE SERVE PARA DESLIZAR UM REGISTRO
+  Widget buildItem(BuildContext context, int index) {
     return Dismissible(
       key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
       background: Container(
@@ -135,6 +138,32 @@ class _HomeState extends State<Home> {
           });
         },
       ),
+      onDismissed: (direction) {
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.removeAt(index);
+
+          _saveData();
+
+          //FUNCAO PARA DESFAZER A EXLUSAO
+          final snack = SnackBar(
+            content: Text("Tarefa \"${_lastRemoved["title"]}\" removida!"),
+            action: SnackBarAction(
+              label: "Desfazer",
+              onPressed: () {
+                setState(() {
+                  _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                  _saveData();
+                });
+              },
+            ),
+            duration: Duration(seconds: 2),
+          );
+
+          Scaffold.of(context).showSnackBar(snack);
+        });
+      },
     );
   }
 }
